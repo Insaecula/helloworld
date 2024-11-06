e2bf4b5ed8378bdeb6e451205d5336393214f5a3
Insaecula <107352917+Insaecula@users.noreply.github.com>
2024-11-06 12:47:23 +0300

    hw-8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 //task1
+        int [] numbers  = new int []{1, 2, 3};
+        double [] fractions = {1.57 , 7.654 , 9.986};
+        int [] myint = new int[]{91 , 95 , 105 };
+        //task2
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+        for (int i = 0; i < fractions.length; i++){
+            System.out.println(fractions[i]);
+        }
+        for (int i = 0;i < myint.length; i++){
+            System.out.println(myint[i]);
+        }
+        //task3
+        for (int i = numbers.length - 1;i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+        for (int i = fractions.length - 1; i >= 0; i--) {
+            System.out.println(fractions[i]);
+        }
+        //task4
+        int[] arr = new int[] {1 , 2 , 3};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i] += 1;
             }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
         }
 
