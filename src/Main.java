303212d78edae0fdfa320467f41c58f4377e7a2b
Insaecula <107352917+Insaecula@users.noreply.github.com>
2024-11-07 15:02:13 +0300

    hw-8

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,49 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 //task1
-        int [] numbers  = new int []{1, 2, 3};
-        double [] fractions = {1.57 , 7.654 , 9.986};
-        int [] myint = new int[]{91 , 95 , 105 };
+        int[] numbers = new int[]{1, 2, 3};
+        double[] fractions = {1.57, 7.654, 9.986};
+        int[] myint = new int[]{91, 95, 105};
         //task2
-        for (int i = 0; i < numbers.length; i++){
-            System.out.println(numbers[i]);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i != myint.length - 1) {
+                System.out.print(", ");
+            }
         }
-        for (int i = 0; i < fractions.length; i++){
-            System.out.println(fractions[i]);
+        System.out.println();
+        for (int i = 0; i < fractions.length; i++) {
+            System.out.print(fractions[i]);
+            if (i != fractions.length - 1) {
+                System.out.print(" ,");
+            }
         }
-        for (int i = 0;i < myint.length; i++){
-            System.out.println(myint[i]);
+        System.out.println();
+        for (int i = 0; i < myint.length; i++) {
+            System.out.print(myint[i]);
+            if (i != myint.length - 1) {
+                System.out.print(" ,");
+            }
         }
+
+        System.out.println();
         //task3
-        for (int i = numbers.length - 1;i >= 0; i--) {
-            System.out.println(numbers[i]);
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
+        System.out.println();
         for (int i = fractions.length - 1; i >= 0; i--) {
-            System.out.println(fractions[i]);
+            System.out.print(fractions[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
         }
+        System.out.println();
         //task4
-        int[] arr = new int[] {1 , 2 , 3};
+        int[] arr = new int[]{1, 2, 3};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0) {
                 arr[i] += 1;
@@ -35,5 +56,5 @@ public class Main {
         System.out.println(Arrays.toString(arr));
 
     }
-        }
+}
 
