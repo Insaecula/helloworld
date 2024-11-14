c500204802456c905249eee931fb9c4ff55aeb90
Insaecula <107352917+Insaecula@users.noreply.github.com>
2024-11-14 15:20:42 +0300

    hw-9

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//task1
+        String firstName = "Ivanon";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О сотрудника " + fullName);
+        //task 2
+        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета -  " + fullName.toUpperCase());
 
 
-            }
-        }
+        //task3
+        fullName = "Иванов Семён Семёнович";
+        String nameEmployee = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + nameEmployee);
 
+
+    }
+}
