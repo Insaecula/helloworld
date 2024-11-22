27362a846884cb614139ab5a3e4b588933b720bc
Insaecula <107352917+Insaecula@users.noreply.github.com>
2024-11-22 16:02:55 +0300

    hw-9

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private  String title;
+    private int publicationYear;
+    private Author author;
+
+    public Book(String title, int publicationYear, Author author) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return getPublicationYear();
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Leo ", "Tolstoy");
+        Author author2 = new Author("Fyodor ", "Dostoevsky");
+
+        Book book1 = new Book("Война и мир", 1869, author1);
+        Book book2 = new Book("Преступление и наказание", 1842, author2);
+
+        book1.setPublicationYear(1870);
 
 
 
