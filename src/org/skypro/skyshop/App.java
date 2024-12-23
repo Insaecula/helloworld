4e5a07251a89d6e187087ef73edbb380a84aa760
Insaecula <107352917+Insaecula@users.noreply.github.com>
2024-12-23 23:56:12 +0300

    hw

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -5,7 +5,7 @@ import org.skypro.skyshop.basket.ProductBasket;
 
 import org.skypro.skyshop.product.Product;
 
-
+//
 public class App {
     public static void main(String[] args) {
         Product product1 = new Product("Яблоко", 70);
