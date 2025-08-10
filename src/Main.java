b45d7d98e92c308c78f8fc5c79107bee94ed20e8
insaecula <morozzzik228@gmail.com>
2025-08-10 12:13:57 +0300

    s

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
+    public static List<Integer> process(int[] arr) {
+        return IntStream.of(arr)
+                .map(n -> % 2 == 0 ? n + 1 : n)
+                .skip(2)
+                .limit(5)
+                .boxed()
+                .toList();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] nums = {1, 2, 4, 5, 6, 8, 10, 12};
+        System.out.println(process(nums));
     }
 }
