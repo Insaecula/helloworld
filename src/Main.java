02482267ef5559a401a7458f722754bda7cd29ec
insaecula <morozzzik228@gmail.com>
2025-08-21 19:33:05 +0300

    tex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String[] arr = (" apple ","banana", "apple", "orange", "banana", "pear");
+        Set<String> set = new LinkedHashSet<>(Arrays.asList(arr));
+        String result = String.join(" ", set);
+        System.out.println(result);
     }
+
 }
